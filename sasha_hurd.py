0708a136cde4e6b7d87fa40dac069e23c4ff0289
DezzenFlo <44757085+DezzenFlo@users.noreply.github.com>
2019-07-21 21:40:43 +0300

    Add files via upload

---
+++ sasha_hurd.py
@@ -0,0 +1,18 @@
+import psutil
+import pyautogui
+
+pyautogui.click(26, 190,duration=1)
+pyautogui.click(112, 9,duration=1)
+pyautogui.click(908, 234,duration=1)
+
+pyautogui.click(766, 1046,duration=1)
+pyautogui.typewrite("Nikita lox",interval=0.25)
+pyautogui.click(1206, 1039,duration=1)
+
+pyautogui.click(541, 225, duration=1)
+pyautogui.click(889, 401, duration=1)
+pyautogui.click(782, 1052, duration=1)
+#pyautogui.click(807,891, duration=1)
+#pyautogui.click()
+pyautogui.typewrite("Nikita Lox", interval=0.25)
+pyautogui.click(1206, 1039, duration=1)
