3c89e7ef0b6f42088cc9109bc267b6e6e38d17b8
DezzenFlo <44757085+DezzenFlo@users.noreply.github.com>
2019-07-21 15:21:59 +0300

    Add files via upload

---
+++ alarm_clock.py
@@ -0,0 +1,24 @@
+import datetime
+import time
+import winsound
+
+now = datetime.datetime.now()
+
+datetime.time(now.hour, now.minute, now.second)
+
+first = now.hour
+three = now.minute
+secondt = now.second
+
+
+#a= int(input(": "))
+b= int(input("Через сколько вы хотите встать(в секундах): "))
+
+min = b * 60
+time.sleep(min)
+
+temp = 500
+while True:
+
+	winsound.Beep(temp, 100)
+	temp = temp + 10
