1e160f40fac4630d95e1a25de170003765130363
Sasimaxim123 <53124939+Sasimaxim123@users.noreply.github.com>
2019-08-23 23:26:30 +0300

    Update (time)timer.py

--- (time)timer.py
+++ (time)timer.py
@@ -2,24 +2,27 @@ import datetime
 import time
 import winsound
 
-now = datetime.datetime.now()
+def timer():
 
-datetime.time(now.hour, now.minute, now.second)
+  now = datetime.datetime.now()
 
-first = now.hour
-three = now.minute
-secondt = now.second
+  datetime.time(now.hour, now.minute, now.second)
+
+  first = now.hour
+  three = now.minute
+  secondt = now.second
 
 
 
-b= int(input("Через какое время вы хотите поставить таймер (в минутах): "))
+  b= int(input("Через какое время вы хотите поставить таймер (в минутах): "))
 
-min = b * 60
-time.sleep(min)
+  min = b * 60
+  time.sleep(min)
 
-winsound.Beep(3000,100)
-winsound.Beep(2500,100)
-winsound.Beep(2000,100)
-winsound.Beep(1500,100)
-winsound.Beep(1000,100)
-winsound.Beep(500,100)
+  winsound.Beep(3000,100)
+  winsound.Beep(2500,100)
+  winsound.Beep(2000,100)
+  winsound.Beep(1500,100)
+  winsound.Beep(1000,100)
+  winsound.Beep(500,100)
+timer()
